3de9c732d6fb476a14e504e2630a69597c549475
Jonas Stein <news@jonasstein.de>
2010-09-27 15:28:19 +0200

    heute soll Auctex laufen

--- .emacs.el
+++ .emacs.el
@@ -96,3 +96,16 @@
 ;;   dotmode
 ;;   emacs-goodies-el
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(graphviz-dot-indent-width 4)
+ '(graphviz-dot-view-command "geeqie %s"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
