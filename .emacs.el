4905d29d1d829be0b84a7dfa960cef33fff9cf15
Jonas Stein <news@jonasstein.de>
2011-01-09 14:57:22 +0100

    ac URL

--- .emacs.el
+++ .emacs.el
@@ -3,6 +3,7 @@
 ;; 
 
 ;; Auto-Complete
+;; http://cx4a.org/software/auto-complete/manual.html#Installation
 ;(require 'auto-complete)
 ;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (require 'auto-complete-config)
