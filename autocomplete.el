a2214aaa32d9e3c690b1eb9ecb33fe6b1b6daeb6
Jonas Stein <news@jonasstein.de>
2011-01-17 23:05:56 +0100

    config auslagern

---
+++ autocomplete.el
@@ -0,0 +1,7 @@
+;; Auto-Complete
+;; http://cx4a.org/software/auto-complete/manual.html#Installation
+;(require 'auto-complete)
+;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
+(require 'auto-complete)
