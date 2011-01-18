57c01faf5f1baac8d86f80cdc2008ce5f7224677
Jonas Stein <news@jonasstein.de>
2011-01-18 03:37:21 +0100

    READMEs are nice

--- .emacs.el
+++ .emacs.el
@@ -9,7 +9,7 @@
 ;; Face-style
 
 (setq inhibit-splash-screen t) 
-(set-face-font 'default "-Misc-Fixed-Medium-R-Normal--13-120-75-75-C-80-ISO8859-1")
+(set-face-font 'default "-Misc-Fixed-Medium-R-Normal--16-120-75-75-C-80-ISO8859-1")
 
 
 ;; http://www.emacswiki.org/cgi-bin/wiki/FrameTitle 
--- auctex.el
+++ auctex.el
@@ -1,7 +1,16 @@
 ;; AUCTeX 
 
+(setq TeX-auto-save t)
+(setq-default TeX-master nil)
 
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+;; (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
+(add-hook 'TeX-mode-hook 'auto-complete-mode)
 
 
 
