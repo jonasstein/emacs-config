55cb6476ef345492bcd90fae08efecf511026a67
Jonas Stein <news@jonasstein.de>
2011-01-18 03:41:54 +0100

    flyspell global

--- auctex.el
+++ auctex.el
@@ -4,7 +4,6 @@
 (setq-default TeX-master nil)
 
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-;; (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
