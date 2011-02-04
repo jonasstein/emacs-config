0af416ccd95cc7fd64fc5b9dfceac01ae30e1cde
Jonas Stein <jonas.stein@uni-koeln.de>
2011-02-04 16:26:30 +0100

    Gnuplot support added

---
+++ gnuplot.el
@@ -0,0 +1,6 @@
+(autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
+(autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
+(setq auto-mode-alist
+      (append '(("\\.gp$" . gnuplot-mode))
+	      auto-mode-alist))
+(global-set-key [(f9)] 'gnuplot-make-buffer)
