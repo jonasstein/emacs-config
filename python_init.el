fc53cf534e007626801cb707a67f35bc4a0f4441
mfa <andreas@madflex.de>
2011-04-16 17:10:11 +0200

    add a few python addons

--- python_init.el
+++ python_init.el
@@ -24,6 +24,24 @@ if __name__ == \"__main__\":
           )))
 
 
+;; mark lines with width > 80 in python-mode
+(add-hook 'python-mode-hook
+  (lambda ()
+    (font-lock-add-keywords nil
+      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
 
 
+;; on word-delete don't delete ' -- not only in Python
+(modify-syntax-entry ?\' "$")
 
+;; Highlight "FIXME" and "TODO" comments  --  not only in Python
+(defun add-fixme-highlighting ()
+    "Turn on extra highlighting for 'FIXME' and the like."
+    (font-lock-add-keywords nil
+     '(("\\<\\(FIXME\\|TODO\\)" 1 font-lock-warning-face t))))
+(add-hook 'find-file-hooks 'add-fixme-highlighting t)
+
+
+;; pymacs
+;; ropemacs
+;; flymake mit pylint
