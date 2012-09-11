97d16b866bfd8481bb29405ec9aba8e3d0a27bcd
Jonas Stein <news@jonasstein.de>
2012-09-11 15:38:49 +0200

    add elpa

---
+++ elpa/ess-20120906.1549/ess-pkg.el
@@ -0,0 +1,2 @@
+(define-package "ess" "20120906.1549" "No description available. [source: github]" 'nil)
+
---
+++ elpa/pep8-1.2/pep8-autoloads.el
@@ -0,0 +1,27 @@
+;;; pep8-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (pep8) "pep8" "pep8.el" (20554 19554 0 0))
+;;; Generated autoloads from pep8.el
+
+(autoload 'pep8 "pep8" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pep8-pkg.el") (20554 19554 344481 417000))
+
+;;;***
+
+(provide 'pep8-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pep8-autoloads.el ends here
