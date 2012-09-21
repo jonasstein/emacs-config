96b6abbe7174cd6f42062a18c8799c9e3d99569f
Jonas Stein <jonas.stein@uni-koeln.de>
2012-09-21 21:35:05 +0200

    yas

---
+++ elpa/yas-jit-0.8.3/yas-jit-autoloads.el
@@ -0,0 +1,28 @@
+;;; yas-jit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (yas/jit-compile-dir) "yas-jit" "yas-jit.el" (20570
+;;;;;;  7079 0 0))
+;;; Generated autoloads from yas-jit.el
+
+(autoload 'yas/jit-compile-dir "yas-jit" "\
+Compiles directory into a \"bundle\".  Useful for caching purposes.
+
+\(fn DIR)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("yas-jit-pkg.el") (20570 7079 415822 255000))
+
+;;;***
+
+(provide 'yas-jit-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; yas-jit-autoloads.el ends here
