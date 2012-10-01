1eb09292c8e0bded12b5b3a72ae5d9e9d067dc68
Jonas Stein <news@jonasstein.de>
2012-10-01 23:48:53 +0200

    graphviz activated

--- graphviz.el
+++ graphviz.el
@@ -1,3 +1,6 @@
 
 (setq '(graphviz-dot-indent-width 4))
 (setq '(graphviz-dot-view-command "geeqie %s"))
+
+(eval-after-load 'graphviz-dot-color-keywords (setq graphviz-dot-color-keywords (mapcar (lambda (color) (concat (rx bow) (symbol-name color) (rx eow)))   graphviz-dot-colors-(list ))) )
+ 
